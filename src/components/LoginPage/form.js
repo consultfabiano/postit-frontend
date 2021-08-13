8f65d9fe7422b74b99e3d26aa89127f076e6e922
Fabiano Garcia <fabianojp06@gmail.com>
2021-08-13 05:03:19 -0300

    integração back e front

--- src/components/LoginPage/form.js
+++ src/components/LoginPage/form.js
@@ -5,32 +5,55 @@ import Input from '../Inputs/input'
 import Button from '../Buttons/button'
 
 const StyledForm = styled.form`
-margin-top 15px;
-display: flex;
-flex-direction: column;
-align-items: center;
-
+    margin-top 15px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
 `
 
 const StyledText = styled.a`
-margin-top 15px;
-font-size: 14px;
-color: white;
-
+    margin-top 15px;
+    font-size: 14px;
+    color: white;
 `
 
-function Form ({ onSubmit, submitButtonText, linkText, onLinkClick }) {
+const Error = styled.div `
+    color: ${({theme})=> theme.colors.error};
+    text-align: center;
+`
+
+function Form ({ onSubmit, submitButtonText, linkText, onLinkClick, isRegister, error }) {
 
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
+    const [firstName, setFirstName] = useState('')
+    const [lastName, setLastName] = useState('')
 
     const handleSubmit = (event) => {
         event.preventDefault()
-        onSubmit({email, password})
+        onSubmit({email, password, firstName, lastName})
     }
 
     return (
         <StyledForm onSubmit={handleSubmit} >
+            {
+                isRegister && <Input 
+                type='text' 
+                placeholder='Seu nome' 
+                required
+                value= {firstName}
+                onChange={({target}) => setFirstName(target.value)}
+                 /> 
+            }
+            {
+                isRegister && <Input 
+                type='text' 
+                placeholder='Sobrenome' 
+                required
+                value= {lastName}
+                onChange={({target}) => setLastName(target.value)}
+                 /> 
+            }
             <Input 
                 type='email' 
                 placeholder='E-mail' 
@@ -39,14 +62,15 @@ function Form ({ onSubmit, submitButtonText, linkText, onLinkClick }) {
                 onChange={({target}) => setEmail(target.value)}
                  /> 
             <Input 
-            type='password' 
-            placeholder='Senha' 
-            required 
-            value= {password}
-            onChange={({target}) => setPassword(target.value)}
+                type='password' 
+                placeholder='Senha' 
+                required 
+                value= {password}
+                onChange={({target}) => setPassword(target.value)}
             />
             <Button type='submit'>{ submitButtonText }</Button>
             <StyledText href="#" onClick={ onLinkClick }>{ linkText }</StyledText> 
+            {error && <Error>Erro ao {isRegister ? 'Cadastrar' : 'Entrar'}</Error>}
 
         </StyledForm>
     )
--- src/components/LoginPage/formSpace.js
+++ src/components/LoginPage/formSpace.js
@@ -1,21 +1,44 @@
 import { useState } from 'react'
+import { useCookies } from 'react-cookie'
+import { useRouter } from 'next/router'
 
 import Image from 'next/image'
 import styled from 'styled-components'
+import axios from 'axios'
 
 import Form from './form'
+
+const { NEXT_PUBLIC_API_URL } = process.env
 
 const StyledFormSpace = styled.div`
     margin-top: -28px;
 `
 function FormSpace () {
+    const router = useRouter()
+    const [cookie, setCookie] = useCookies(['authorization'])
     const [formState, setFormState] = useState(true)
+    const [error, setError]= useState()
 
     const handleLogin = ({ email, login }) => {
+        setError(false)
         console.log({email, login})
     }
-    const handleRegister = ({ email, password }) => {
-        console.log({email, password})
+    const handleRegister = async ({ email, password, firstName, lastName }) => {
+        setError(false)
+        try{
+            const { data } = await axios.post( `${NEXT_PUBLIC_API_URL}/user/create`, {
+                email, password, firstName, lastName
+            })
+            handleAuth(data.authorization)
+        } catch (err) {
+            console.error(err)
+            console.error(err.message)
+        }
+    }
+
+    const handleAuth = (token) => {
+        setCookie('authorization', token)
+        router.push('/dashboard')
     }
 
 
@@ -30,6 +53,7 @@ function FormSpace () {
                 submitButtonText='Entrar'
                 linkText='Não possui conta? Clique aqui!'
                 onLinkClick= {()=> setFormState (false)}
+                error={ error }
             /> :
 
             <Form 
@@ -37,6 +61,8 @@ function FormSpace () {
                 submitButtonText='Cadastrar'
                 linkText='Já possui conta? Clique aqui!'
                 onLinkClick= {()=> setFormState (true)}
+                isRegister
+                error={ error }
             />
             }
         </StyledFormSpace>
